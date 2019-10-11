a92450e0eda1e552721ab1577ba7c58a8b39b5ef
Damon Self <damongself@gmail.com>
2019-10-11 15:31:36 -0600

    Added registration option for new users to DB

---
+++ backend/app/controllers/registration_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationController < ApplicationController
+skip_before_action :authenticate_request
+
+  def create
+    salt = 'q34tf234'
+    encrypted_password = ''
+
+    if params[:password] != params[:password_confirmation]
+      render 'invalid credentials'
+    else
+      encrypted_password = BCrypt::Password.create(params[:password] + salt)
+    end
+
+    @user = User.create(
+      email: params[:email],
+      password_digest: encrypted_password
+    )
+  end
+end
