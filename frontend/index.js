23dade0ce263d991c8440568461f0f27e9fa5726
Damon Self <damongself@gmail.com>
2019-10-19 15:40:54 -0600

    starting to implement react. Have full auth working at the moment

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  currentMainContent
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('ready for action');
+})
+
+const currentMainContent = document.querySelector('main');
 const loginForm = document.getElementById('login-submission');
+const loginButton = document.getElementById('login-button');
+const logoutButton = document.getElementById('logout-button');
+const signupButton = document.getElementById('signup-submission');
+
+
+
+
+
+
+
 
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  emailSubmission = document.getElementById('email');
-  passwordSubmission = document.getElementById('password');
+  const emailSubmission = document.getElementById('login-email');
+  const passwordSubmission = document.getElementById('login-password');
 
   fetch('http://localhost:3000/authenticate', {
     method: "POST",
@@ -19,3 +42,56 @@ loginForm.addEventListener('submit', (e) => {
     .then(response => response.json())
     .then(token => localStorage.setItem('auth_token', `${token.auth_token}`));
 });
+
+signupButton.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const emailSubmission = document.getElementById('signup-email');
+  const passwordSubmission = document.getElementById('signup-password');
+  const passwordConfirmationSubmission = document.getElementById('signup-password_confirmation');
+
+  fetch('http://localhost:3000/registration', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      email: emailSubmission.value,
+      password: passwordSubmission.value,
+      password_confirmation: passwordConfirmationSubmission.value
+    })
+  })
+})
+
+loginButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  destroyPreviousContent();
+  
+});
+
+logoutButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.removeItem('auth_token')
+})
+
+
+
+
+
+
+
+function destroyPreviousContent() {
+  loginForm.style.display = 'none';
+};
+
+function renderHome() {
+
+};
+
+function loginRender() {
+  
+};
+
+function signupRender() {
+
+};
