4022ff75cd3634c3ee728444acf04771902049aa
Damon Self <damongself@gmail.com>
2019-10-25 10:54:07 -0600

    Implements basic Cards condtional rendering - The Cards nav bar li now has an event listener that listens for a click. When clicked, it changes the currentPage state by invoking the loadCardPage action passed down as a prop from App. - Once the state of currentPage has been changed to 'cards' the check will render the Cards prop rather than the previously rendered prop. - The cards prop, once rendered, returns the Card viewing ul and the card zone div with all the images of each card. - Each image has an event listener that, when clicked, renders the selected card object's data as li's in the ul card display.

--- mtg-react-frontend/src/Cards/Cards.js
+++ mtg-react-frontend/src/Cards/Cards.js
@@ -2,17 +2,16 @@ import React, {Component} from 'react';
 import AllCards from '../AllCards/AllCards'
 
 export default class Cards extends Component {
-render() {
-  return(
-    <div className="cardRenderZone">
-      <section id="line-break"></section>
-      <ul className="currentCard"></ul> 
-      <div className="card-zone">
-        {this.state && this.state.magic_cards &&
-          <AllCards magic_cards={this.state.magic_cards} sampleCards={this.state.sampleCards} currentCardDisplay={this.currentCardDisplay}/>
-        }
+  render() {
+    return(
+      <div className="cardRenderZone">
+        <ul className="currentCard"></ul> 
+        <div className="card-zone">
+          {this.props.magic_cards &&
+            <AllCards magic_cards={this.props.magic_cards} sampleCards={this.props.sampleCards} currentCardDisplay={this.props.currentCardDisplay}/>
+          }
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
